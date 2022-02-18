f9345b482de4a0fcd391164feb21d5c07ef35d4c
Sorin <work@csorin.com>
2022-02-18 12:29:17 +0100

    feat: add home and blog links

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,4 +7,21 @@ export default theme({
   head: {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
+  i18n: {
+    locales: () => [
+      {
+        code: "fr",
+        iso: "fr-FR",
+        file: "fr-FR.js",
+        name: "Français",
+      },
+      {
+        code: "en",
+        iso: "en-US",
+        file: "en-US.js",
+        name: "English",
+      },
+    ],
+    defaultLocale: "en",
+  },
 });
